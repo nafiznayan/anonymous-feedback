9f421f45a5d1ea5100c58e986ae0dda623113110
Nafiznayan <abdullah191864@gmail.com>
2025-09-09 12:31:27 +0600

    Created db model and connected to db

---
+++ src/model/User.ts
@@ -0,0 +1,66 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessages: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verification code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verification code expiry is required"],
+  },
+  isVerified: { type: Boolean, default: false },
+  isAcceptingMessages: { type: Boolean, default: true },
+  messages: [MessageSchema],
+});
+
+export const MessageModel = mongoose.model<Message>("Message", MessageSchema);
+
+/**
+ * UserModel:
+ * - Checks if a User model already exists in `mongoose.models` to prevent
+ *   `OverwriteModelError` during Next.js hot reloading.
+ * - If not found, creates a new model using the UserSchema.
+ */
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+export default UserModel;
